1193977779864f8a0bc71b42be5b9592258c5a6a
Ivan Kuznetsov <kuzma.wm@gmail.com>
2014-11-06 17:05:37 +0200

    Fix timezone issue
    There is an issue with timezone in <pubDate>.
    
    Instead of
    
    ```
    <pubDate>Wed, 05 Nov 2014 20:14:04 +0000</pubDate>
    ```
    
    it should looks like
    
    ```
    <pubDate>Wed, 05 Nov 2014 22:14:04 +0200</pubDate>
    ```

--- feed.php
+++ feed.php
@@ -3,6 +3,9 @@ header('Content-Type: ' . feed_content_type('rss-http') . '; charset=' . get_opt
 $more = 1;
 $options = get_option( 'yandex_news' );
 
+$gmt_offset = get_option('gmt_offset');
+$gmt_offset = ($gmt_offset > 9) ? $gmt_offset.'00' : ('0'.$gmt_offset.'00');
+
 echo '<?xml version="1.0" encoding="'.get_option('blog_charset').'"?'.'>'; ?>
 
 <rss version="2.0"
@@ -24,7 +27,7 @@ echo '<?xml version="1.0" encoding="'.get_option('blog_charset').'"?'.'>'; ?>
 		<title><?php the_title_rss() ?></title>
 		<link><?php the_permalink_rss() ?></link>
 		<author><?php the_author() ?></author>
-		<pubDate><?php echo mysql2date('D, d M Y H:i:s +0000', get_post_time('Y-m-d H:i:s', true), false); ?></pubDate>
+		<pubDate><?php echo mysql2date('D, d M Y H:i:s +'.$gmt_offset, get_date_from_gmt(get_post_time('Y-m-d H:i:s', true)), false); ?></pubDate>
 	<?php $content = get_the_content_feed('rss2'); ?>
 		<yandex:full-text><?php echo esc_textarea( strip_tags( $content ) ); ?></yandex:full-text>
 	</item>
